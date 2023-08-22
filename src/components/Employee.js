9f1ab8a61e3dbcca4d22c814f3b1c276a40e0a6e
Muhammed Sobhi <mohamed.sophy89847@outlook.com>
2023-08-22 13:12:14 +0300

    before todos

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,18 +1,24 @@
 const Employee = (props) => {
-  const data = props.data
+  const userData = props.data;
   return (
-    <ul className="list p-3 bg-gradient bg-secondary-subtle rounded-4">
-      {data.map((ele)=>{
-        return(
-          <li key={ele.id} className="rounded-2 p-4 ps-2" style={{"borderColor":`${ele.job==="Developer"?("black"):(ele.job==="UX/UI Designer"?("green"):("yellow"))}`}}>
-            <h3>{ele.name}</h3>
-            <small className="fst-italic font-monospace p-1">{ele.job}</small>
-            <i style={{"color":"brown"}}>{ele.isActive===true? ("Active"):("Inactive")}</i>
-          </li>
-        )
-      })}
-    </ul>
-  )
-}
+    <li
+      className='rounded-2 p-4 ps-2'
+      style={{
+        borderColor: `${
+          userData.job === "Developer"
+            ? "black"
+            : userData.job === "UX/UI Designer"
+            ? "green"
+            : "yellow"
+        }`,
+      }}>
+      <h3>{userData.name}</h3>
+      <small className='fst-italic font-monospace p-1'>{userData.job}</small>
+      <i style={{ color: "brown" }}>
+        {userData.isActive === true ? "Active" : "Inactive"}
+      </i>
+    </li>
+  );
+};
 
-export default Employee
+export default Employee;
